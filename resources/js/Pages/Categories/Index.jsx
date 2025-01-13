9a2b391f64e8efa2f29a29129441283d35704796
Vynvan <68636193+Vynvan@users.noreply.github.com>
2025-01-14 00:56:11 +0100

    Update authorization in UpdateCategoryRequest and enhance input field in Categories index

--- resources/js/Pages/Categories/Index.jsx
+++ resources/js/Pages/Categories/Index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import CategoryList from '@/Components/CategoryList';
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { useForm, Head } from '@inertiajs/react';
-import CategoryList from '@/Components/CategoryList';
 
 export default function Index({ auth, categories }) {
     const { data, setData, post, processing, reset, errors } = useForm({
@@ -24,7 +24,7 @@ export default function Index({ auth, categories }) {
 
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
-                    <input value={data.name} onChange={e => setData('name', e.target.value)} type="text" name="name" id="name" required
+                    <input value={data.name} onChange={e => setData('name', e.target.value)} placeholder='Name' type="text" name="name" id="name" required
                         className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                     />
                     <InputError message={errors.message} className="mt-2" />
