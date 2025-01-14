d07dcb7ffe49c46c7b2b8c17ed068b29ab3e02b2
Vynvan <68636193+Vynvan@users.noreply.github.com>
2025-01-14 02:13:33 +0100

    Bugfixed CategoryList

--- resources/js/Components/CategoryList.jsx
+++ resources/js/Components/CategoryList.jsx
@@ -4,10 +4,10 @@ const CategoryList = ({ categories }) => {
    return (
       <div>
          <ul>
-            {categories.map(category => (
-               <li key={category.id} className='flex justify-between items-center'>
+            {categories && categories.map(category => (
+               <li key={category.id} className='mt-1 p-2 flex justify-between items-center'>
                   <span className="text-gray-800 font-semibold">{category.name}</span>
-                  <small className="ml-2 text-sm text-gray-600">(created by {category.createdBy.name})</small>
+                  <small className="ml-2 text-sm text-gray-600">created by {category.created_by.name}</small>
                </li>
             ))}
          </ul>
